c13df92ead483053fbb17527909ae785908143db
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-22 13:05:03 -0600

    update base model to run validation

--- src/app/models/Model.php
+++ src/app/models/Model.php
@@ -6,86 +6,121 @@ use DB\SQL\Mapper;
 
 class Model extends Mapper
 {
-	public $attributes = [];
-	public $validationRules;
-	public $filterRules;
-	public $errors;
+    public $attributes = [];
+    public $validationRules;
+    public $filterRules;
+    public $errors;
 
     /**
      * @return array
      */
-    public function toEndPoint(){
-		$attributes = [];
-		foreach ($this->attributes as $attribute) {
-			$attributes[$attribute] = $this->{$attribute};
-		}
-		return $attributes;
-	}
+    public function toEndPoint()
+    {
+        $attributes = [];
+        foreach ($this->attributes as $attribute) {
+            $attributes[$attribute] = $this->{$attribute};
+        }
+        return $attributes;
+    }
 
     /**
      * @param $attribute
      * @param $value
-     * @return array
      * @throws \Exception
      */
-    public function findByAttribute($attribute, $value){
-		$this->load(["{$attribute}=?", $value]);
-		if ($this->dry()) {
-		    throw new \Exception("Not found.", 404);
-        } else {
-            return $this->query;
+    public function findByAttribute($attribute, $value)
+    {
+        $this->load(["{$attribute}=?", $value]);
+        if ($this->dry()) {
+            throw new \Exception("Not found.", 404);
         }
-	}
+    }
 
     /**
-     * @return array
+     * @return void
      */
     public function all()
-	{
-		$this->load();
-		return $this->query;
-	}
+    {
+        $this->load();
+    }
 
     /**
      * @param $id
      * @param $values
+     * @param bool $runValidation We give the option of skipping validation, otherwise lets always run it.
+     * @return bool
+     * @throws \Exception
      */
-    public function edit($id, $values)
-	{
-		$this->load(['id=?', $id]);
-		$this->copyFrom($values);
-		$this->update();
-	}
+    public function edit($id, $values, $runValidation = true)
+    {
+        $this->load(['id=?', $id]);
+        if ($runValidation) {
+            $validated = $this->validate($values);
+            if (!is_array($validated)) {
+                $this->_edit($values);
+            } else {
+                $this->errors = $validated;
+                return false;
+            }
+        } else {
+            $this->_edit($values);
+            return true;
+        }
+    }
+
+    /**
+     * Internal edit so I dont have to repeat myself.
+     * @param $values
+     */
+    private function _edit($values) {
+        $this->copyFrom($values);
+        $this->update();
+    }
 
     /**
      * @param $values
+     * @return bool
+     * @throws \Exception
      */
-    public function create($values)
-	{
-		$this->copyFrom($values);
-		$this->save();	
-	}
+    public function create($values, $runValidation = true)
+    {
+        if ($runValidation) {
+            $validated = $this->validate($values);
+            if (!is_array($validated)) {
+                $this->copyFrom($values);
+                $this->save();
+            } else {
+                $this->errors = $validated;
+                return false;
+            }
+        }
+    }
 
     /**
      * @param $values
      * @return array|bool|null
      * @throws \Exception
+     * @TODO I am thinking that we may want to throw an error if this fails.
      */
     public function validate($values)
-	{
-		$validator = new \GUMP;
-		$values = $validator->sanitize((array)$values);
-		$validator->validation_rules($this->validationRules);
-		$validator->filter_rules($this->filterRules);
+    {
+        $validator = new \GUMP;
+        $values = $validator->sanitize((array)$values);
+        $validator->validation_rules($this->validationRules);
+        $validator->filter_rules($this->filterRules);
 
-		$validatedData = $validator->run($values);
+        $validatedData = $validator->run($values);
 
         return $validatedData === false ? $validator->get_errors_array() : true;
-	}
+    }
 
-	public function delete($id)
-	{
-		$this->load(['id' => $id]);
-		$this->erase();
-	}
+    /**
+     * Deletes a model from the database.
+     * @param $id
+     */
+    public function delete($id)
+    {
+        $this->load(['id' => $id]);
+        $this->erase();
+    }
 }
